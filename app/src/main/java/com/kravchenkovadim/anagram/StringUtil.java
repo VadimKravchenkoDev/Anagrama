8b81e862c8c975cd5652be1461e30a3485b62e68
Arhizmei <arhizmei@gmail.com>
2024-11-16 16:06:16 +0200

    use split in StringUtil

--- app/src/main/java/com/kravchenkovadim/anagram/StringUtil.java
+++ app/src/main/java/com/kravchenkovadim/anagram/StringUtil.java
@@ -5,54 +5,50 @@ import java.util.Set;
 
 public class StringUtil {
 
-    public StringBuilder makeAnagram(String inputSymbols, String filterString) {
+    public String makeAnagram(String inputSymbols, String filterString) {
+        // We divide the input row into words
+        String[] words = inputSymbols.split(" ");
+
         StringBuilder result = new StringBuilder();
-        StringBuilder currentWord = new StringBuilder();
-        char space = ' ';
-        
-        // Loop through each character in inputSymbols
-        for (int a = 0; a < inputSymbols.length(); a++) {
-            currentWord.append(inputSymbols.charAt(a));
-            // If we find a space, process the current word
-            if (inputSymbols.charAt(a) == space) {
-                // Remove the space from the end of the current word
-                currentWord.deleteCharAt(currentWord.length() - 1);
-                // Reverse current word with the filterString applied, and add to result
-                result.append(doReverse(currentWord, filterString)).append(space);
-                currentWord.setLength(0); // Clear for the next word
-            }
+
+        for (String word : words) {
+            // Invertable skin word, use filter
+            result.append(doReverse(new StringBuilder(word), filterString)).append(" ");
         }
-        // Process the last word, as it doesn't end with a space
-        result.append(doReverse(currentWord, filterString));
-        return result;
+
+        // delete last space
+        if (result.length() > 0) {
+            result.deleteCharAt(result.length() - 1);
+        }
+
+        return result.toString();
     }
+
 
     //do reverse and leave filter symbols on starting place
     public StringBuilder doReverse(StringBuilder word, String filter) {
         StringBuilder result = new StringBuilder();
 
-        // Initialize filterString with spaces, same length as word
+        //initialize the row to save the position of filtering symbols
         StringBuilder filterString = new StringBuilder(" ".repeat(word.length()));
 
         outerLoop:
         for (int i = word.length() - 1; i >= 0; i--) {
-
-            //Find filtering symbols and add them into filterString
+            // check the every symbols that is in the filter
             for (int j = 0; j < filter.length(); j++) {
-                //find a filter symbol in word
                 if (word.charAt(i) == filter.charAt(j)) {
                     filterString.setCharAt(i, word.charAt(i));
                     continue outerLoop;
                 }
             }
-            //add reverse symbol without filtering symbols
+            // Symbols that are not filterable can be added
             result.append(word.charAt(i));
         }
+
+        // Add filtering symbols at the correct position
         for (int i = 0; i < filterString.length(); i++) {
             for (int j = 0; j < filter.length(); j++) {
                 if (filterString.charAt(i) == filter.charAt(j)) {
-
-                    //add filtering symbols in correct place
                     result.insert(i, filterString.charAt(i));
                 }
             }
